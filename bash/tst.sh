d499c53177ebc05d9e0f5fd009a0ece819bfcff4
Michael Kenney <michael.kenney@returnpath.com>
2018-04-16 23:17:37 -0600

    git related cleanup

---
+++ bash/tst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ~/repos/src/github.com/mkenney/git-status/git-status.sh
+status=$(__git_status)
+exit_code=$?
+
+echo "status:    '$status'"
+echo "exit_code: '$exit_code'"
+
+exit $exit_code
