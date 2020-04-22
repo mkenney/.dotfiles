015e4795eba888320340661c472bdb25a045ae3e
Michael Kenney <mkenney@webbedlam.com>
2020-04-21 19:08:25 -0600

    updates

--- .tmux.btt.sh
+++ .tmux.btt.sh
@@ -3,9 +3,22 @@
 SESSION=default
 echo $(tmux list-windows -t $SESSION | awk '{print $2}')
 
-if [ "" = "$(tmux list-windows -t default | grep ‘can\\\'t find session’)" ]; then echo 'n/a'; else echo $(tmux list-windows -t $SESSION | awk '{print $2}'); fi
+if [ "" = "$(tmux list-windows -t default | grep "can't find session")" ]; then
+    echo 'n/a'
+else
+    echo $(tmux list-windows -t $SESSION | awk '{print $2}')
+fi
 
-windows=$(tmux list-windows -t $SESSION | awk '{print $2}’) \
-$(echo $windows | grep ‘can't find session’ > /dev/null); if [ “0” = “$?” ]; then echo $windows; else; echo ’n/a’; fi
+windows=$(tmux list-windows -t $SESSION | awk '{print $2}')
+$(echo $windows | grep "can't find session" > /dev/null)
+if [ "0" = "$?" ]; then
+    echo $windows;
+else
+    echo 'n/a'
+fi
 
-if [ "" != "$(/usr/local/bin/tmux list-windows -t default | grep "can't find session")" ]; then echo 'n/a'; else echo $(/usr/local/bin/tmux list-windows -t default | awk '{print $2}'); fi
+if [ "" != "$(/usr/local/bin/tmux list-windows -t default | grep "can't find session")" ]; then
+    echo 'n/a'
+else
+    echo $(/usr/local/bin/tmux list-windows -t default | awk '{print $2}')
+fi
