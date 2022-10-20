9271b59bbc13f3ec19d80d60c6a2817a0e3d840d
Michael Kenney <mkenney@webbedlam.com>
2022-10-20 13:13:03 -0600

    save

--- .tmux.sh
+++ .tmux.sh
@@ -5,28 +5,26 @@ IFS=$'\n\t'
 
 SESSION=default
 
-/usr/local/bin/tmux -2 attach-session -t $SESSION
+env tmux -2 attach-session -t $SESSION
 if [ $? != 0 ]; then
     echo "initializing default session..."
 
-    /usr/local/bin/tmux -2 new-session -d -s $SESSION
+    env tmux -2 new-session -d -s $SESSION
 
-    /usr/local/bin/tmux rename-window     -t $SESSION:0 'scratch'
-    /usr/local/bin/tmux new-window        -t $SESSION:1
-    /usr/local/bin/tmux new-window        -t $SESSION:2
-    /usr/local/bin/tmux new-window        -t $SESSION:3
-    /usr/local/bin/tmux new-window        -t $SESSION:4
-    /usr/local/bin/tmux new-window        -t $SESSION:5
+    env tmux rename-window     -t $SESSION:0 'scratch'
+    env tmux new-window        -t $SESSION:1
+    env tmux new-window        -t $SESSION:2
+    env tmux new-window        -t $SESSION:3
+    env tmux new-window        -t $SESSION:4
+    env tmux new-window        -t $SESSION:5
 
     # set recently used order
-    tmux select-window                    -t $SESSION:5
-    tmux select-window                    -t $SESSION:4
-    tmux select-window                    -t $SESSION:3
-    tmux select-window                    -t $SESSION:2
-    tmux select-window                    -t $SESSION:1
-    tmux select-window                    -t $SESSION:0
+    env tmux select-window                    -t $SESSION:5
+    env tmux select-window                    -t $SESSION:4
+    env tmux select-window                    -t $SESSION:3
+    env tmux select-window                    -t $SESSION:2
+    env tmux select-window                    -t $SESSION:1
+    env tmux select-window                    -t $SESSION:0
 
-    __startWatcher
-
-    /usr/local/bin/tmux -2 attach-session -t $SESSION
+    env tmux -2 attach-session -t $SESSION
 fi
