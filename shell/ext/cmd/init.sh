ccbccc7ccd2cb5aa6695552700296fbcdd758c2b
Michael Kenney <michael.kenney@validity.com>
2024-01-02 22:48:45 -0700

    save

--- shell/ext/cmd/init.sh
+++ shell/ext/cmd/init.sh
@@ -1,10 +1,14 @@
-export PATH="$(dirname $0):$PATH"
-
+#
 # extend any tool with additional commands
+#
+
+export PATH="$(dirname $0):$PATH"
 export __EXT_DIR=${HOME}/.dotfiles/shell/ext
 
-# load all of the custom commands in bash/ext/
+# enable tab completion for aliased commands
 setopt COMPLETE_ALIASES
+
+# load all of the custom commands in bash/ext/
 for ext in $(find $__EXT_DIR -maxdepth 1 -type d -and -not -name '.*'); do
     if [ "$ext" != "$__EXT_DIR" ] && [ "$ext" != "$__EXT_DIR/cmd" ]; then
         # alias the command to the __ext_cmd wrapper method
