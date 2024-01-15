bbcfb57f973c5680105e3948d00ab7688d9b0097
Michael Kenney <michael.kenney@validity.com>
2024-01-14 17:05:34 -0700

    save

--- shell/prompt/prompt.zsh
+++ shell/prompt/prompt.zsh
@@ -17,28 +17,29 @@ precmd() {
     # k8s status
     k8s_status=$(__k8s_status)
     if [ "" != "$k8s_status" ]; then
-        tool_states+=("%{${COLOR_DKGREEN_FADED}%}%{${K8S_PS1_SYMBOL}%G%} ${k8s_status}%{${COLOR_NORM}%}")
+        tool_states+=("%{${COLOR_DKGREEN_FADED}%}| %{${K8S_PS1_SYMBOL}%G%} ${k8s_status}%{${COLOR_NORM}%}")
     fi
 
     # git status
     in_git_repo=$(/usr/bin/env git rev-parse --is-inside-work-tree 2> /dev/null)
     if [ "true" = "$in_git_repo" ]; then # In a git repo
-        tool_states+=("%{${COLOR_BLUE_FADED}%}$(echo $(__git_status))%{${COLOR_NORM}%}")
+        tool_states+=("%{${COLOR_BLUE_FADED}%}| $(echo $(__git_status))%{${COLOR_NORM}%}")
     fi
 
     # define midline (tool states)
     if [ "" != "$tool_states" ]; then
+        topline="┌ %{${COLOR_YELLOW_FADED}%}20%D%{${COLOR_NORM}%} %d ${tool_states}"
         #statusline=$(printf "\n│ ${tool_states}")
-        statusline=" ${tool_states}"
-    else
-        statusline=
+    #     statusline=" ${tool_states}"
+    # else
+    #     statusline=
     fi
 
     # define command prompt
     if [ "0" = "$last_exit_code" ] || [ "" = "$last_exit_code" ]; then
-        promptline="└ %{${COLOR_GREEN_FADED}%}%D{%H:%M:%S}%{${COLOR_NORM}%}${statusline} → %{$(echo -e -n "\x1b[\x35 q")%}"
+        promptline="└ %{${COLOR_GREEN_FADED}%}%D{%H:%M:%S}%{${COLOR_NORM}%} → %{$(echo -e -n "\x1b[\x35 q")%}"
     else
-        promptline="└ %{${COLOR_GREEN_FADED}%}%D{%H:%M:%S}%{${COLOR_NORM}%}${statusline} (%{${COLOR_RED_FADED}%}${last_exit_code}%{${COLOR_NORM}%}) ⤳ %{$(echo -e -n "\x1b[\x35 q")%}"
+        promptline="└ %{${COLOR_GREEN_FADED}%}%D{%H:%M:%S}%{${COLOR_NORM}%} (%{${COLOR_RED_FADED}%}${last_exit_code}%{${COLOR_NORM}%}) ⤳ %{$(echo -e -n "\x1b[\x35 q")%}"
     fi
 }
 
